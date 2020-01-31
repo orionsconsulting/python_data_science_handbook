a40250cdfc7bc62762bd35a9b01dd849b82f35c8
thomas williams <thomas.williams@ark.gov>
2020-01-30 18:16:38 -0600

    Matplotlib

---
+++ chapter_4/myplot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.linspace(0, 10, 100)
+
+fig = plt.figure()
+plt.plot(x, np.sin(x), '-')
+plt.plot(x, np.cos(x), '--')
+
+fig.savefig('my_figure.png')
+
